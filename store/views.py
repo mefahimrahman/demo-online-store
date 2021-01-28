d7101a37d2ce58c9cade37d11ad7a520b7e53854
mefahimrahman <mefahimrahman@gmail.com>
2021-01-28 16:27:07 +0600

    Render Products in Template
    we use views.py to fetch the product data from database and render it to
    store.html

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
 
 
 def cart(request):
